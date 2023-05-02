354501035ddfe2d97cb7647a5ff4885cf0f57ed5
3PILLAR\peter.drevicky <peter.drevicky@gmail.com>
2023-05-02 11:20:55 +0200

    add emotion library, replace home.modules.css with emotion styles components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,8 +2,7 @@ import { useAuthContext } from "../../hooks/useAuthContext";
 import { useCollection } from "../../hooks/useCollection";
 import TransactionList from "./TransactionList";
 import React from "react";
-// styles
-import styles from "./Home.module.css";
+import styled from "@emotion/styled";
 
 // components
 import TransactionForm from "./TransactionForm";
@@ -18,14 +17,73 @@ export default function Home(): ReactElement {
   );
 
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>
+    <Container>
+      <Content>
         {error && <p>{error}</p>}
         {documents && <TransactionList transactions={documents} />}
-      </div>
-      <div className={styles.sidebar}>
+      </Content>
+      <Sidebar>
         <TransactionForm uid={user?.uid ?? ""} />
-      </div>
-    </div>
+      </Sidebar>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  max-width: 960px;
+  margin: 60px auto;
+`;
+
+const Content = styled.div`
+  padding-right: 30px;
+`;
+
+const Sidebar = styled.div`
+  padding-left: 30px;
+  form {
+    padding: 20px;
+    background: #1f9751;
+    border-radius: 10px;
+  }
+  input,
+  select {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    margin-top: 8px;
+    box-sizing: border-box;
+    border: 0;
+    border-radius: 4px;
+    color: #555;
+    font-size: 1em;
+  }
+  label {
+    margin: 0 auto 20px;
+    display: block;
+    color: #fff;
+  }
+  button {
+    color: #fff;
+    border: 2px solid #fff;
+    padding: 6px 12px;
+    background-color: transparent;
+    font-size: 1em;
+    border-radius: 4px;
+    cursor: pointer;
+    display: block;
+    width: 100%;
+  }
+  h3 {
+    color: #1f9751;
+    margin-bottom: 20px;
+  }
+  aside {
+    margin-top: 40px;
+    color: #555;
+  }
+  aside li {
+    margin: 10px;
+  }
+`;
--- src/pages/home/TransactionList.tsx
+++ src/pages/home/TransactionList.tsx
@@ -1,9 +1,7 @@
 import React, { ReactElement } from "react";
 import { useFirestore } from "../../hooks/useFirestore";
-
-// styles
-import styles from "./Home.module.css";
 import { Transactions } from "../../types/types";
+import styled from "@emotion/styled";
 
 interface Props {
   transactions: Transactions;
@@ -13,16 +11,57 @@ export default function TransactionList({ transactions }: Props): ReactElement {
   const { deleteDocument } = useFirestore("transactions");
 
   return (
-    <ul className={styles.transactions}>
+    <TransactionsContainer>
       {transactions.map((transaction) => {
         return (
           <li key={transaction.id}>
-            <p className={styles.name}>{transaction.name}</p>
-            <p className={styles.amount}>${transaction.amount}</p>
+            <Name>{transaction.name}</Name>
+            <Amount>${transaction.amount}</Amount>
             <button onClick={() => deleteDocument(transaction.id)}>x</button>
           </li>
         );
       })}
-    </ul>
+    </TransactionsContainer>
   );
 }
+
+const TransactionsContainer = styled.ul`
+  li {
+    margin: 30px auto;
+    border: 1px solid #f2f2f2;
+    box-shadow: 3px 3px 5px rgba(50, 50, 50, 0.1);
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    overflow: hidden;
+    border-left: 4px solid #1f9751;
+  }
+
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: #ddd;
+    color: #777;
+    border: none;
+    padding: 12px 8px;
+    text-align: center;
+    line-height: 0;
+    font-size: 0.9em;
+    cursor: pointer;
+  }
+`;
+
+const Name = styled.p`
+  color: #777;
+  font-size: 1.4em;
+`;
+
+const Amount = styled.p`
+  margin-left: auto;
+  margin-right: 40px;
+  color: #777;
+  font-weight: bold;
+  font-size: 1.6em;
+`;
